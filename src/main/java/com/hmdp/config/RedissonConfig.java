de2d36e047797a97505a9e041025862f83cbc93f
lycnight <lycnight@gmail.com>
2024-11-02 08:37:17 +0800

    multiLock 联锁，解决 redis 集群主从一致问题

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -10,12 +10,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
+    // redis 集群-节点1
     @Bean
     public RedissonClient redissonClient(){
         // 配置
         Config config = new Config();
         config.useSingleServer()
                 .setAddress("redis://8.152.211.25:6379")
+                .setPassword("linux369");
+        // 创建 RedissonClient 对象
+        return Redisson.create(config);
+    }
+
+    // redis 集群-节点2
+    @Bean
+    public RedissonClient redissonClient2(){
+        // 配置
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://8.152.211.25:6380")
+                .setPassword("linux369");
+        // 创建 RedissonClient 对象
+        return Redisson.create(config);
+    }
+
+    // redis 集群-节点3
+    @Bean
+    public RedissonClient redissonClient3(){
+        // 配置
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://8.152.211.25:6381")
                 .setPassword("linux369");
         // 创建 RedissonClient 对象
         return Redisson.create(config);
