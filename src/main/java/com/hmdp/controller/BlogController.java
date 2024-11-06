21767f19c804114fc44572414128ecb88ea0df76
lycnight <lycnight@gmail.com>
2024-11-06 13:52:15 +0800

    关注推送功能

--- src/main/java/com/hmdp/controller/BlogController.java
+++ src/main/java/com/hmdp/controller/BlogController.java
@@ -32,13 +32,7 @@ public class BlogController {
 
     @PostMapping
     public Result saveBlog(@RequestBody Blog blog) {
-        // 获取登录用户
-        UserDTO user = UserHolder.getUser();
-        blog.setUserId(user.getId());
-        // 保存探店博文
-        blogService.save(blog);
-        // 返回id
-        return Result.ok(blog.getId());
+        return blogService.saveBlog(blog);
     }
 
     @PutMapping("/like/{id}")
@@ -72,5 +66,33 @@ public class BlogController {
     public Result queryBlogLikes(@PathVariable("id") Long id){
         return blogService.queryBlogLikes(id);
     }
+
+    @GetMapping("/of/user")
+    // 根据用户id查询该用户的博客列表
+    // 这样写表示查询参数
+    // https://127.0.0.1:8080/of/user?id=3&current=5
+    // 这个URL表示查询用户ID为3的用户的博客列表的第五页。
+    public Result queryBlogByUserId(
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam("id") Long id
+    ){
+        // 根据用户id查询
+        // 使用page方法进行分页查询，创建一个Page对象，
+        // 其中包含当前页码和每页的最大记录数（由SystemConstants.MAX_PAGE_SIZE常量定义）
+        Page<Blog> page = blogService.query().eq("user_id", id).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        // 从Page对象中获取当前页的博客记录列表。
+        List<Blog> records = page.getRecords();
+        return Result.ok(records);
+    }
+
+    @GetMapping("/of/follow")
+    public Result queryBlogOfFollow(
+            @RequestParam("lastId") Long max,
+            @RequestParam(value = "offset", defaultValue = "0") Integer offset
+    ){
+        return blogService.queryBlogOfFollow(max, offset);
+    }
+
 
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -17,4 +31,79 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private IUserService userService;
+
+    @Override
+    // 判断是否关注
+    public Result isFollow(Long followUserId) {
+        // 1. 获取登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 2. 查询是否关注
+        // select count(*) from tb_follow where user_id = ? and follow_user_id = ?
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
+        // 3. 判断
+        return Result.ok(count > 0);
+    }
+
+    @Override
+    // 关注该用户 or 取关该用户
+    public Result follow(Long followUserId, Boolean isFollow) {
+        // 0. 获取登录用户，并构造 Redis 集合的 key
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:" + userId;
+
+        // 1. 判断到底是关注还是取关
+        if(isFollow){
+            // 2. 关注，新增数据
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            boolean isSuccess = save(follow);// 向数据库 tb_follow 表中添加一条记录
+            if(isSuccess){
+                // 把关注用户的id，放入redis的set集合中
+                // sadd userId followerUserId
+                // followerUserId 表示 userId 所关注的用户（们）的id
+                stringRedisTemplate.opsForSet().add(key, followUserId.toString());
+            }
+        } else{
+            // 3. 取关， 删除数据
+            // delete from tb_follow where user_Id = ? and follow_user_id = ?
+            boolean isSuccess = remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId)
+                    .eq("follow_user_id", followUserId));
+            if(isSuccess){
+                // 把关注的用户的id从 Redis 集合中移除
+                stringRedisTemplate.opsForSet().remove(key, followUserId.toString());
+            }
+        }
+        //
+        return Result.ok();
+    }
+
+    @Override
+    public Result followCommons(Long id) {
+        // 1. 获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:" + userId;
+        // 2. 求交集
+        String key2 = "follows:" + id;
+        Set<String> intersect = stringRedisTemplate.opsForSet().intersect(key, key2);
+        // 3. 解析 id 集合
+        if(intersect == null || intersect.isEmpty()){
+            return Result.ok(Collections.emptyList());
+        }
+        List<Long> ids = intersect.stream().map(Long::valueOf).collect(Collectors.toList());
+        // 4. 查询用户并转换为 UserDTO
+        List<UserDTO> users = userService.listByIds(ids)
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+
+        // 返回结果
+        return Result.ok(users);
+    }
 }
